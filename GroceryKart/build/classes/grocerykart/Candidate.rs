e585eadd6ff5cab9b1e6dfda72223fdcec5a2788
Deepank Kartikey <deepankkartikey@gmail.com>
2018-04-06 22:01:47 +0530

    first commit

---
+++ GroceryKart/build/classes/grocerykart/Candidate.rs
@@ -0,0 +1,2 @@
+grocerykart.Route
+grocerykart.Candidate
---
+++ GroceryKart/build/classes/grocerykart/welcome.rs
@@ -0,0 +1,8 @@
+grocerykart.welcome
+grocerykart.welcome$2
+grocerykart.welcome$1
+grocerykart.welcome$6
+grocerykart.welcome$5
+grocerykart.welcome$4
+grocerykart.welcome$3
+grocerykart.GroceryKartSavedData
